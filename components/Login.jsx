4bfec3a9aa83335ce506bdf236b5fc354b273767
sunyrora <sunyrora@gmail.com>
2022-07-09 15:33:20 +0200

    react-form-hook for login page

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,33 +1,71 @@
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 function Lgoin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = ({ email, password }) => {
+    console.log('email: ', email, 'password: ', password);
+  };
+
   return (
     <div className="flex items-center justify-center rounded-lg border-gray-200 shadow-md w-full p-5 m-5">
-      <form action="" className="flex flex-col justify-center max-w-xl">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-center max-w-xl"
+      >
         <h1 className="mb-5 text-xl text-center">Login</h1>
-        <div className="login-form flex space-x-5">
-          <div className="mb-4 space-y-2 flex flex-col items-start">
-            <label htmlFor="email">Email</label>
-            <label htmlFor="password">Password</label>
-          </div>
-          <div className="mb-4 space-y-2 flex flex-col items-start">
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              className="w-full border-b"
-              autoFocus
-            />
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              className="w-full border-b"
-            />
+        <div className="login-form ">
+          <div className="inline-grid grid-cols-4 gap-5 overflow-x-auto items-center">
+            <label className="col-span-1" htmlFor="email">
+              Email
+            </label>
+            <div className="col-span-3 p-1">
+              <input
+                type="email"
+                {...register('email', {
+                  required: 'Please enter email',
+                  pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                })}
+                id="email"
+                placeholder="Enter your email"
+                className="w-full border-b"
+                autoFocus
+              />
+              {errors.email && (
+                <div className="form-error-message">{errors.email.message}</div>
+              )}
+            </div>
+            <label className="col-span-1" htmlFor="password">
+              Password
+            </label>
+            <div className="col-span-3 p-1">
+              <input
+                type="password"
+                {...register('password', {
+                  required: 'Please enter password',
+                  minLength: {
+                    value: 6,
+                    message: 'password is more than 5 chars',
+                  },
+                })}
+                id="password"
+                placeholder="Enter your password"
+                className="w-full border-b"
+              />
+              {errors.password && (
+                <div className="form-error-message">
+                  {errors.password.message}
+                </div>
+              )}
+            </div>
           </div>
         </div>
-        <div className="primary-button">Login</div>
-        <div className="my-3">
+        <button className="primary-button my-5">Login</button>
+        <div>
           {`Don't have an account? `}
           <Link href="register">
             <a className="font-bold">Register</a>
