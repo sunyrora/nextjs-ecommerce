b1828b1d736437e88808df8477a1f9dfefe053e0
sunyrora <sunyrora@gmail.com>
2022-07-10 22:42:09 +0200

    sample user data

---
+++ utils/sampleUserData.js
@@ -0,0 +1,20 @@
+export const userData = [
+  {
+    username: 'first user',
+    email: 'first@abc.com',
+    password: '123456',
+    isAdmin: false,
+  },
+  {
+    username: 'user2',
+    email: 'second@abc.com',
+    password: '123456',
+    isAdmin: false,
+  },
+  {
+    username: 'admin',
+    email: 'admin@admin.com',
+    password: '123456',
+    isAdmin: true,
+  },
+];
